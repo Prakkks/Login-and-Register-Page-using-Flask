5ff71143af3527fc20564e25cff40c94570a501f
Prakkks <tprakrity@gmail.com>
2024-05-08 14:00:21 +0545

    Db add form data into db done successfully

--- app.py
+++ app.py
@@ -6,10 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 # import bcrypt
 import secrets
 from sqlalchemy import Column, String, Integer , MetaData, Table, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker , session
 
 
 # engine = db.create_engine('mysql://root:1234@localhost:3306/loginproject',echo=True)
 secret_key = secrets.token_hex(16)
+
+
 
 app = Flask(__name__)
 
@@ -18,24 +22,38 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/loginp
 
 db = SQLAlchemy(app)
 
+class Lient(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
 
 @app.route('/')
-def index(): 
-    return "hi"
+def add_data():
+    # Create an instance of the Lient class with the data you want to add
+    # new_entry = Lient(name='John', email='john@example.com')
+
+    # # Add the instance to the database session and commit
+    # db.session.add(new_entry)
+    # new_entry = Lient(name='Prakriti', email='prak@gmail.com')
+    # db.session.add(new_entry)
+    # db.session.commit()
+
+    return 'Data added successfully'
+
+
 
 @app.route('/register', methods=['GET','POST'])
-def register():
-    metadata= MetaData()
-    metadata.reflect(bind=db.engine)
-    for table in metadata.tables.values():
-        print(f"Table: {table.name}")
-        for column in table.c:
-            print(f"Column: {column.name}, Type: {column.type}")
-        print()
-    if request.method == 'POST'  :
-        username= request.form['Name']
-        email = request.form['Email']
-        pass
+def register():  
+    Name=''
+    Emails=''  
+    if request.method == 'POST':
+        
+        Name= request.form['Name']
+        Emails= request.form['Email']
+        new_entry = Lient(name=Name,email=Emails)
+        db.session.add(new_entry)
+        db.session.commit()
+        return render_template('register.html', Name= Name, Email = Emails )
         
 
     return render_template('register.html')
