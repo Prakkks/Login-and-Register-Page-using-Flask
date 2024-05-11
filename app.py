e182f7c726aabc235c0dde70bcf64e1136badb73
Prakkks <tprakrity@gmail.com>
2024-05-11 22:03:38 +0545

    Final update for now

--- app.py
+++ app.py
@@ -4,20 +4,18 @@ from wtforms import StringField, SubmitField,PasswordField
 from wtforms.validators import DataRequired,Email,ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import bcrypt
-import secrets
 from sqlalchemy import Column, String, Integer , MetaData, Table, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker , session
 
 
 # engine = db.create_engine('mysql://root:1234@localhost:3306/loginproject',echo=True)
-secret_key = secrets.token_hex(16)
 
 
 
 app = Flask(__name__)
 # bcrypt = Bcrypt(app)
-app.config['SECRET_KEY'] = secret_key
+# app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/loginproject'
 
 db = SQLAlchemy(app)
@@ -31,16 +29,7 @@ class Users(db.Model):
 
 @app.route('/')
 def add_data():
-    # Create an instance of the Lient class with the data you want to add
-    # new_entry = Lient(name='John', email='john@example.com')
-
-    # # Add the instance to the database session and commit
-    # db.session.add(new_entry)
-    # new_entry = Lient(name='Prakriti', email='prak@gmail.com')
-    # db.session.add(new_entry)
-    # db.session.commit()
-
-    return 'Data added successfully'
+    return render_template('dashhboard.html')
 
 
 
@@ -73,18 +62,14 @@ def login():
         Emails = request.form['Email'] 
         passwords = request.form['password']
         password=passwords.encode('utf-8')
-        user = Users.query.filter_by(email=Emails).first()
-        
+        user = Users.query.filter_by(email=Emails).first()  
         if user:
             if bcrypt.checkpw(password,user.passwords.encode('utf-8')):
-                # return jsonify({'message': 'Login successful'}), 200
                 return render_template('adminLogin.html', message='Login successful')
             else:
-                # return jsonify({'message': 'Incorrect password'}), 401
                 return render_template('adminLogin.html', message='Incorrect Password')
 
         else:
-            # return jsonify({'message': 'User not found'}), 404
             return render_template('adminLogin.html', message='User not found')
     return render_template('adminLogin.html')
 
