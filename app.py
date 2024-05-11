651065a796d69c9fd166465f66f012381a3958ea
Prakkks <tprakrity@gmail.com>
2024-05-11 21:56:39 +0545

    COmpleted Password check

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField,PasswordField
 from wtforms.validators import DataRequired,Email,ValidationError
 from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
+from flask_bcrypt import bcrypt
 import secrets
 from sqlalchemy import Column, String, Integer , MetaData, Table, create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -16,7 +16,7 @@ secret_key = secrets.token_hex(16)
 
 
 app = Flask(__name__)
-bcrypt = Bcrypt(app)
+# bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:1234@localhost:3306/loginproject'
 
@@ -27,6 +27,7 @@ class Users(db.Model):
     username = db.Column(db.String)
     email = db.Column(db.String)
     passwords=db.Column(db.String)
+    salt =db.Column(db.String, nullable=False)
 
 @app.route('/')
 def add_data():
@@ -50,8 +51,9 @@ def register():
         Name= request.form['Name']
         Emails= request.form['Email']
         hpassword=request.form['Password']
-        hashpassword= bcrypt.generate_password_hash(hpassword).decode('utf-8')
-        new_entry = Users(username=Name,email=Emails,passwords=hashpassword)
+        salt = bcrypt.gensalt()
+        hashpassword= bcrypt.hashpw(hpassword.encode('utf-8'),salt)
+        new_entry = Users(username=Name,email=Emails,passwords=hashpassword,salt=salt)
         all_users = Users.query.filter_by(email=Emails).first()
         if all_users:
             print(all_users)
@@ -70,16 +72,16 @@ def login():
     if request.method == 'POST':
         Emails = request.form['Email'] 
         passwords = request.form['password']
-        
+        password=passwords.encode('utf-8')
         user = Users.query.filter_by(email=Emails).first()
         
         if user:
-            if bcrypt.check_password_hash(Users.passwords, passwords):
+            if bcrypt.checkpw(password,user.passwords.encode('utf-8')):
                 # return jsonify({'message': 'Login successful'}), 200
                 return render_template('adminLogin.html', message='Login successful')
             else:
                 # return jsonify({'message': 'Incorrect password'}), 401
-                return render_template('adminLogin.html', message='Login Unsuccessful')
+                return render_template('adminLogin.html', message='Incorrect Password')
 
         else:
             # return jsonify({'message': 'User not found'}), 404
